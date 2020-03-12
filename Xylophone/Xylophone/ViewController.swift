785764ac55e95d198f6359bcd9e0acd0f3331289
Stefka Vacheva <stefka.vacheva@imperiaonline.org>
2020-03-12 13:40:20 +0200

    init Xylophone project and add app icon

---
+++ Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Stefka Vacheva on 12.03.20.
+//  Copyright © 2020 Stefka Vacheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
