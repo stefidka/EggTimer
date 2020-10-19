e93ee5d8547ae7ce65d3cc3f9707fcb704d4eb0d
Stefka Vacheva <stef.vacheva@imperiaonline.org>
2020-10-19 16:41:37 +0300

    setup launch screen and main screen pf project

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+    }
 }
 
