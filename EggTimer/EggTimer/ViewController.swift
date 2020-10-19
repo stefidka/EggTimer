1f1e422fc785d28e8eea5d0ea29e27c7beee1edf
Stefka Vacheva <stef.vacheva@imperiaonline.org>
2020-10-19 16:23:24 +0300

    init eggtimer project

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Stefka Vacheva on 19.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
