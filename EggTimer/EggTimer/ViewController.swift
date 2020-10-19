5b4fb8a967d6f20744f527c2e6a014e33bccadde
Stefka Vacheva <stef.vacheva@imperiaonline.org>
2020-10-19 18:07:52 +0300

    add timer to eggtimer project

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -8,7 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var titleLabel: UILabel!
+    
+   // let eggTimes = ["Soft" : 300, "Medium" : 420, "Hard" : 720]
+    
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +26,25 @@ class ViewController: UIViewController {
 
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
     }
 }
 
